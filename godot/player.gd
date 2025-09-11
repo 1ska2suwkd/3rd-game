114f456b49c068d677b188d526a54282f787be0d
1ska2suwkd <mickey050110@naver.com>
2025-09-11 10:58:10 +0000

    나무와 던전입구 충돌 설정

--- godot/player.gd
+++ godot/player.gd
@@ -1,31 +1,29 @@
 extends CharacterBody2D
-@export var speed = 400
-var screen_size
 
-# 실행 될 때 한 번 호출 되는 함수 == create 함수
-func _ready():
-	screen_size = get_viewport_rect().size
+@export var speed := 400.0
 
-# update 함수
-func _process(delta):
-	var velocity = Vector2.ZERO
+func _physics_process(delta):
+	var dir := Vector2.ZERO
 	if Input.is_action_pressed("move_up"):
-			velocity.y -= 1
+		dir.y -= 1
 	if Input.is_action_pressed("move_down"):
-			velocity.y += 1
+		dir.y += 1
 	if Input.is_action_pressed("move_left"):
-			velocity.x -= 1
+		dir.x -= 1
 	if Input.is_action_pressed("move_right"):
-			velocity.x += 1
-			
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.animation = "walk"
+		dir.x += 1
+
+	# 방향 벡터 정규화 후 속도 적용
+	if dir != Vector2.ZERO:
+		velocity = dir.normalized() * speed
+		$AnimatedSprite2D.play("walk")
 		if velocity.x < 0:
 			$AnimatedSprite2D.flip_h = true
 		elif velocity.x > 0:
 			$AnimatedSprite2D.flip_h = false
 	else:
+		velocity = Vector2.ZERO
 		$AnimatedSprite2D.play("idle")
-		
-	position += velocity * delta
+
+	# ❗ 충돌 계산 포함된 이동
+	move_and_slide()
