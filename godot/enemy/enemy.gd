2d33346ddfbe43b90456a27108f77d3efb5cf8f9
mickey050110 <mickey050110@naver.com>
2025-09-25 17:23:57 +0900

    게임 방식을 보스 1회 격파로 변경, 그에 따라 적을 skull에서 troll로 변경, 플레이어가 인식 범위 안에 들어가면 troll이 플레이어를 따라오는 기능 구현

--- godot/enemy/enemy.gd
+++ godot/enemy/enemy.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends CharacterBody2D
 
 var stat: Stat
 var playerstat : PlayerStat
@@ -7,21 +7,37 @@ var player_chase = false
 var player = null
 
 func _ready():
-	stat = Stat.new(200, 6, 2)
+	stat = Stat.new(100,6, 2)
 	
-func _physics_process(delta):
+func _physics_process(_delta):
 	if player_chase:
 		position += (player.position - position) / stat.speed
+		$AnimatedSprite2D.play("walk")
+		if (player.position.x - position.x) < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.play("idle")
 
-func _on_area_entered(area):
+func _on_area_2d_area_entered(area):
 	if area.is_in_group("Player_attack"):
 		$AnimatedSprite2D.modulate = Color(0.847, 0.0, 0.102)
 	playerstat = PlayerStat.new()
 	stat.take_damage(playerstat.damage)
 	if stat.dead:
-		queue_free()
+		queue_free()# Replace with function body.
 
-func _on_area_exited(area):
+func _on_area_2d_area_exited(area):
 	if area.is_in_group("Player_attack"):
 		$AnimatedSprite2D.modulate = Color(1.0, 1.0, 1.0, 1.0)
-	
+
+func _on_detection_area_body_entered(body):
+	if body.is_in_group("player"):
+		player = body
+		player_chase = true
+
+func _on_detection_area_body_exited(body):
+	if body.is_in_group("player"):
+		player = null
+		player_chase = false
