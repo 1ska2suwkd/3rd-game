5f3dc7d9fa0cede650f81ba50a44d16fb5f955a0
1ska2suwkd <mickey050110@naver.com>
2025-10-05 15:01:22 +0000

    1. 기존 Dungeon.tscn을 Boss.tscn으로 수정 2. 새로운 Dungeon.tscn을 만들고 tilelayer로 Dungeon 꾸미기

--- godot/StartScene.gd
+++ godot/StartScene.gd
@@ -14,6 +14,6 @@ func _on_area_2d_body_exited(body):
 func change_scene():
 	if global.transition_scene == true:
 		if global.current_scene == "StartScene" and Input.is_action_just_pressed("Interaction"):
-			get_tree().change_scene_to_file("res://Dungeon.tscn")
+			get_tree().change_scene_to_file("res://Dungeon/Dungeon.tscn")
 			global.finish_change_scenes()
  
