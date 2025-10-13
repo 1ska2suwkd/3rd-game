8efbbccd7fe114631435cfde111fa152df200f49
1ska2suwkd <mickey050110@naver.com>
2025-10-13 11:59:36 +0000

    chest 쉐이더 추가 플레이어가 다가가면 쉐이더 활성화

--- godot/Dungeon/Chest/chest.gd
+++ godot/Dungeon/Chest/chest.gd
@@ -1,8 +1,27 @@
 extends StaticBody2D
 
+@onready var outline_material := preload("res://Dungeon/Chest/chest.tres")
+@onready var sprite := $AnimatedSprite2D
 
+var ready_open = false
+
+func _ready() -> void:
+	sprite.material = null
+
+
+func _process(_delta: float) -> void:
+	if ready_open and Input.is_action_just_pressed("Interaction"):
+		$AnimatedSprite2D.play("open")
+		sprite.material = null
+	
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player") and Input.is_action_just_pressed("Interaction"):
-		$AnimatedSprite2D.play("open")
-		
+	if body.is_in_group("player"):
+		ready_open = true
+		sprite.material = outline_material
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		ready_open = false
+		sprite.material = null
