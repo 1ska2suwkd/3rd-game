a93ad9e72414ff7feadea859793dec754e01ee51
1ska2suwkd <mickey050110@naver.com>
2025-11-03 11:40:35 +0000

    master: 대화 UI 추가.

--- godot/StartScene/StartScene.gd
+++ godot/StartScene/StartScene.gd
@@ -1,17 +1,42 @@
 extends Node2D
 
+var ready_master = false
+var ready_store = false
+
 func _process(_delta):
-	if global.transition_scene == true:
+	if global.transition_scene:
 		if Input.is_action_just_pressed("Interaction"):
 			global.change_scene("res://Dungeon/Room/Room0.tscn")
+	elif ready_master:
+		pass
+	elif ready_store:
+		pass
 
- 
-func _on_area_2d_body_entered(body):
+# Dungeon 메서드
+func _on_area_2d_body_entered(body): 
 	if body.is_in_group("player"):
 		global.transition_scene = true
-
-
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if Input.is_action_just_pressed("Interaction"): return
 	if body.is_in_group("player"):
 		global.transition_scene = false
+
+
+# Master 메서드
+func _on_talk_to_master_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		ready_master = true
+func _on_talk_to_master_body_exited(body: Node2D) -> void:
+	if Input.is_action_just_pressed("Interaction"): return
+	if body.is_in_group("player"):
+		ready_master = false
+		
+
+# Store 메서드
+func _on_entered_store_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		ready_store = true
+func _on_entered_store_body_exited(body: Node2D) -> void:
+	if Input.is_action_just_pressed("Interaction"): return
+	if body.is_in_group("player"):
+		ready_store = false
