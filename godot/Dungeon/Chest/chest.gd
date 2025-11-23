8821362bc75510ddb83b6d20e3deb2184686415b
1ska2suwkd <mickey050110@naver.com>
2025-11-23 08:10:42 +0000

    아이템 나오는 애니메이션 추가

--- godot/Dungeon/Chest/chest.gd
+++ godot/Dungeon/Chest/chest.gd
@@ -2,12 +2,12 @@ extends StaticBody2D
 
 @onready var outline_material := preload("res://UI/Outline.tres")
 @onready var sprite := $AnimatedSprite2D
+@onready var inItem = preload("res://Resources/Items/Scene/ItemScene.tscn").instantiate()
 
 var ready_open = false
 var opend = false
 
 func _ready() -> void:
-	randomize()
 	outline_material.set_shader_parameter("outline_size", 1)
 	sprite.material = null
 
@@ -19,14 +19,14 @@ func _process(_delta: float) -> void:
 		OpenChest()
 
 func OpenChest():
-	var RandomItemNumber = randi_range(1,4)
-	
 	$AnimatedSprite2D.play("open")
+	await $AnimatedSprite2D.animation_finished
 	sprite.material = null
 	opend = true
 	
+	inItem.global_position = global_position
+	get_tree().current_scene.add_child(inItem)
 	
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player") and not opend:
 		ready_open = true
--- godot/Resources/Items/add_Item.gd
+++ godot/Resources/Items/add_Item.gd
@@ -2,20 +2,27 @@ extends Area2D
 
 #@export var exdata: Resource
 @onready var Sprite = $Sprite2D
+@onready var spawn_animation = $AnimationPlayer
 var item_number: int
 var item_data: Resource
 var item_script
 
-#func _ready() -> void:
-	#Setup(exdata)
+func _ready() -> void:
+	randomize()
 
-func Setup(data: Resource):
-	item_data = data
+	Setup()
+
+func Setup():
+	var RandomItemNumber = randi_range(1,4)
+	
+	item_data = AllItem.all_item[RandomItemNumber]
 	
 	Sprite.texture = item_data.sprite
 	item_number = item_data.item_number
 	if item_data.effect_script:
 		item_script = item_data.effect_script.new()
+		
+	spawn_animation.play("ItemSpawn")
 
 func use_item():
 	if item_script:
