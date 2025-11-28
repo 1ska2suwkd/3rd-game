6cb7912573093c0bb4386a72a7ef5a96a670c5e5
1ska2suwkd <mickey050110@naver.com>
2025-11-28 13:04:36 +0900

    Update Room.gd
    리펙토링

--- godot/Dungeon/Room/Room.gd
+++ godot/Dungeon/Room/Room.gd
@@ -4,24 +4,20 @@ extends Node2D
 @onready var transtion_animation = $Dungeon_Scene_Manager/animation
 
 func _ready() -> void:
+	$Ysort/player.position.x = global.player_position_x
+	$Ysort/player.position.y = global.player_position_y
+	$Ysort/player/AnimatedSprite2D.flip_h = global.player_flip_h
+	
 	match global.player_position_x:
-		-680.0: 
-			transtion_animation.play("east_faid_out")
-			await transtion_animation.animation_finished
-			global.is_stop = false
+		-680: 
+			SceneReady("east_faid_out")
 		680: 
-			transtion_animation.play("west_faid_out")
-			await transtion_animation.animation_finished
-			global.is_stop = false
+			SceneReady("west_faid_out")
 	match global.player_position_y:
 		345: 
-			transtion_animation.play("north_faid_out")
-			await transtion_animation.animation_finished
-			global.is_stop = false
+			SceneReady("north_faid_out")
 		-305: 
-			transtion_animation.play("south_faid_out")
-			await transtion_animation.animation_finished
-			global.is_stop = false
+			SceneReady("south_faid_out")
 	
 		
 	print(global.room_count,"번째 방")
@@ -32,10 +28,6 @@ func _ready() -> void:
 			var method_name = "_on_" + door_name + "_body_entered"
 			if not area.is_connected("body_entered", Callable(self, method_name)):
 				area.connect("body_entered", Callable(self, method_name))
-				
-	$Ysort/player.position.x = global.player_position_x
-	$Ysort/player.position.y = global.player_position_y
-	$Ysort/player/AnimatedSprite2D.flip_h = global.player_flip_h
 
 func check_enemies():
 	var enemies = get_tree().get_nodes_in_group("enemy")
@@ -58,9 +50,11 @@ func _on_all_enemies_cleared():
 	$Door_locked/CollisionPolygon2D.disabled = true
 	
 
-func change_room():
+func change_room(direction):
 	if global.transition_scene: return # 중복 방지
 	global.transition_scene = true
+
+	PlayerPositionSetUp(direction)
 	
 	if global.random_number_generator.randf() < global.probabilities[global.chest_room_stack]:
 		global.change_scene("res://Dungeon/Room/Stage1_chest_Room.tscn")
@@ -76,47 +70,53 @@ func change_room():
 		global.room_count += 1
 		#global.chest_room_stack += 1
 		global.change_scene(global.get_random_dungeon_scene())
+		
+func SceneReady(AnimationName):
+	transtion_animation.play(AnimationName)
+	await transtion_animation.animation_finished
+	global.is_stop = false
+
+func SceneEnd(direction, AnimationName):
+	global.is_stop = true
+	transtion_animation.play(AnimationName)
+	await transtion_animation.animation_finished
+	change_room(direction)
+	
+func PlayerPositionSetUp(direction):
+	match direction:
+		"north":
+			global.player_position_x = 0
+			global.player_position_y = 345
+			global.player_flip_h = $Ysort/player/AnimatedSprite2D.flip_h
 			
-
-
+		"south":
+			global.player_position_x = 0
+			global.player_position_y = -305
+			global.player_flip_h = $Ysort/player/AnimatedSprite2D.flip_h
+			
+		"east":
+			global.player_position_x = -680
+			global.player_position_y = -21
+			global.player_flip_h = $Ysort/player/AnimatedSprite2D.flip_h
+			
+		"west":
+			global.player_position_x = 680
+			global.player_position_y = -21
+			global.player_flip_h = $Ysort/player/AnimatedSprite2D.flip_h
+		
+			
 func _on_NorthDoor_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		global.is_stop = true
-		transtion_animation.play("north_faid_in")
-		await transtion_animation.animation_finished
-		change_room()
-		global.player_position_x = 0
-		global.player_position_y = 345
-		global.player_flip_h = $Ysort/player/AnimatedSprite2D.flip_h
+		SceneEnd("north", "north_faid_in")
 		
 func _on_SouthDoor_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		global.is_stop = true
-		transtion_animation.play("south_faid_in")
-		await transtion_animation.animation_finished
-		change_room()
-		global.player_position_x = 0
-		global.player_position_y = -305
-		global.player_flip_h = $Ysort/player/AnimatedSprite2D.flip_h
+		SceneEnd("south", "south_faid_in")
 		
 func _on_EastDoor_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		global.is_stop = true
-		transtion_animation.play("east_faid_in")
-		await transtion_animation.animation_finished
-		
-		change_room()
-		global.player_position_x = -680.0
-		global.player_position_y = -21
-		global.player_flip_h = $Ysort/player/AnimatedSprite2D.flip_h
+		SceneEnd("east", "east_faid_in")
 		
 func _on_WestDoor_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		global.is_stop = true
-		transtion_animation.play("west_faid_in")
-		await transtion_animation.animation_finished
-		
-		change_room()
-		global.player_position_x = 680
-		global.player_position_y = -21
-		global.player_flip_h = $Ysort/player/AnimatedSprite2D.flip_h
+		SceneEnd("west", "west_faid_in")
