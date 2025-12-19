bc0faacd2b31e20146341b14abe78ca125816112
1ska2suwkd <mickey050110@naver.com>
2025-12-19 20:42:41 +0900

    새로운 기능 알아냄!
    타일레이어의 씬을 넣을 수 있다는 사실을 알아내서 잔디와 상자를 수정함

--- godot/Object/Script/CollisionEnemy.gd
+++ godot/Object/Script/CollisionEnemy.gd
@@ -55,15 +55,15 @@ func die():
 		push_warning("⚠️ YSort 노드를 찾을 수 없습니다. current_scene에 직접 추가합니다.")
 		get_tree().current_scene.add_child(particle)
 	
-	if global.random_number_generator.randf() < 0.5:
-		var gold = preload("res://PickUp/gold.tscn").instantiate()
-		gold.global_position = global_position
-		
-		if ysort:
-			ysort.add_child(gold)
-		else:
-			push_warning("⚠️ YSort 노드를 찾을 수 없습니다. current_scene에 직접 추가합니다.")
-			get_tree().current_scene.add_child(gold)
+	#if global.random_number_generator.randf() < 0.5:
+		#var gold = preload("res://PickUp/gold.tscn").instantiate()
+		#gold.global_position = global_position
+		#
+		#if ysort:
+			#ysort.add_child(gold)
+		#else:
+			#push_warning("⚠️ YSort 노드를 찾을 수 없습니다. current_scene에 직접 추가합니다.")
+			#get_tree().current_scene.add_child(gold)
 		
 	queue_free()
 	
