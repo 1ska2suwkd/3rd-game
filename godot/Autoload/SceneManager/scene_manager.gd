31abd7f81e8661c4741a3aac85be386c979620ee
1ska2suwkd <mickey050110@naver.com>
2026-01-03 19:07:39 +0900

    SceneManager.gd가 두개 있어서 하나 삭제함

--- godot/Autoload/SceneManager/scene_manager.gd
+++ godot/Autoload/SceneManager/scene_manager.gd
@@ -13,5 +13,8 @@ func change_scene(scene_path):
 	Scene_Animation.play("transition_in")
 	await Scene_Animation.animation_finished
 	global.is_stop = false
+	
+	#이거 안하면 다음 방에서 할 때 안되더라..
 	Scene_Animation.play("RESET")
+	
 	
