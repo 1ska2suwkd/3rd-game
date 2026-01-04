22747b016b6ad8083c74e108ff7d53b18a709864
1ska2suwkd <mickey050110@naver.com>
2026-01-04 20:41:30 +0900

    몬스터가 다 죽었는 지 확인하는 알고리즘 아직 버그가 있는듯함
    내일 수정하자

--- godot/Components/Common/Death_Component/death_component.gd
+++ godot/Components/Common/Death_Component/death_component.gd
@@ -3,9 +3,8 @@ class_name DeathComponent
 
 	
 func die():
+	EventBus.check_room_clear.emit()
 	
-	if owner.is_in_group("enemy"):
-		remove_from_group("enemy") # queue_free는 다음 프레임의 삭제하기 때문에 그룹에서 직접 제거
 		
 	var ysort = get_tree().current_scene.get_node("Ysort")
 	var particle = preload("res://Particle/EnemyDeadParticle.tscn").instantiate()
@@ -28,6 +27,5 @@ func die():
 			push_warning("⚠️ YSort 노드를 찾을 수 없습니다. current_scene에 직접 추가합니다.")
 			get_tree().current_scene.add_child(gold)
 	
-	EventBus.check_room_clear.emit()
 	
 	owner.call_deferred("queue_free")
