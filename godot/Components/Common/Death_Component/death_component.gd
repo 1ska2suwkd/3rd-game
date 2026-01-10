dfb63a0c189add098ee0d1084541a932b0d4ed27
1ska2suwkd <mickey050110@naver.com>
2026-01-10 14:58:03 +0900

    Dropitem Component 완성

--- godot/Components/Common/Death_Component/death_component.gd
+++ godot/Components/Common/Death_Component/death_component.gd
@@ -10,21 +10,5 @@ func die():
 	
 	EventBus.enemy_die.emit()
 	_die.emit()
-		
-	var ysort = get_tree().current_scene.get_node("Ysort")
-	var particle = preload("res://Particle/EnemyDeadParticle.tscn").instantiate()
-	particle.global_position = global_position
-	
-	
-	if global.random_number_generator.randf() < 0.5:
-		var gold = preload("res://PickUp/gold.tscn").instantiate()
-		gold.global_position = global_position
-		
-		if ysort:
-			ysort.add_child(gold)
-		else:
-			push_warning("⚠️ YSort 노드를 찾을 수 없습니다. current_scene에 직접 추가합니다.")
-			get_tree().current_scene.add_child(gold)
-	
 	
 	owner.call_deferred("queue_free")
---
+++ godot/Components/Enemies/ItemDropComponent/item_drop_component.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@export var death_component: DeathComponent
+@export var drop_item_list: Array[PackedScene]
+@export var drop_chance: float = 0.5
+
+
+func _ready() -> void:
+	death_component._die.connect(drop_item)
+	
+func drop_item():
+	var ysort = get_tree().current_scene.get_node("Ysort")
+	
+	if randf() > drop_chance:
+		return
+		
+	var item_scene = drop_item_list.pick_random()
+	var item_instance = item_scene.instantiate()
+	
+	item_instance.global_position = owner.global_position
+	ysort.add_child(item_instance)
