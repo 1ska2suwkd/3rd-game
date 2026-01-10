efff632c6707119cda23f8c8b21365a722eca02d
1ska2suwkd <mickey050110@naver.com>
2026-01-10 14:11:47 +0900

    DeathParticle Component 완성
    HarpoonFish한테만 적용되어있음

--- godot/Components/Common/Death_Component/death_component.gd
+++ godot/Components/Common/Death_Component/death_component.gd
@@ -2,23 +2,19 @@ extends Node2D
 class_name DeathComponent
 
 var is_die = false
+signal _die()
 
 func die():
 	if is_die: return
 	is_die = true
 	
-	EventBus.enemy_die.emit()	
+	EventBus.enemy_die.emit()
+	_die.emit()
 		
 	var ysort = get_tree().current_scene.get_node("Ysort")
 	var particle = preload("res://Particle/EnemyDeadParticle.tscn").instantiate()
 	particle.global_position = global_position
-	particle.emitting = true
 	
-	if ysort:
-		ysort.add_child(particle)
-	else:
-		push_warning("⚠️ YSort 노드를 찾을 수 없습니다. current_scene에 직접 추가합니다.")
-		get_tree().current_scene.add_child(particle)
 	
 	if global.random_number_generator.randf() < 0.5:
 		var gold = preload("res://PickUp/gold.tscn").instantiate()
---
+++ godot/Components/Enemies/Particle/death_particle_component.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@export var death_component: DeathComponent
+@export var death_particle: PackedScene
+
+
+func _ready() -> void:
+	death_component._die.connect(create_particle)
+	
+func create_particle():
+	var ysort = get_tree().current_scene.get_node("Ysort")
+	var instance = death_particle.instantiate()
+	ysort.add_child(instance)
+	instance.emitting = true
+	instance.global_position = owner.global_position
