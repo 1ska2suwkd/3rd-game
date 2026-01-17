44a705f687666a6049cb0ed8d6bda577ccf6a7f7
1ska2suwkd <mickey050110@naver.com>
2026-01-17 19:19:54 +0900

    Spear_man 추가, 에셋 최신화
    spear_man texture 추가

--- godot/Entities/Master/Skill/Master_Skill.gd
+++ godot/Entities/Master/Skill/Master_Skill.gd
@@ -3,7 +3,7 @@ extends Node
 @onready var Crescent_Slash_item = preload("res://Resources/Items/Crescent_Slash.tres")
 @onready var spear_item = preload("res://Resources/Items/Spear.tres")
 
-var crescnet_count:int = 0
+var crescnet_count:int = 1
 var spear_count:int = 0
 
 var has_crescent: bool = false
