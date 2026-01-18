9ec5b9d997a97e84ecbeb4380e0b4dc86e1d4e91
1ska2suwkd <mickey050110@naver.com>
2026-01-18 15:08:41 +0900

    한번만 대화 가능한 npc 기능 추가

--- godot/Components/Common/Dialogue/Talk/talk_component.gd
+++ godot/Components/Common/Dialogue/Talk/talk_component.gd
@@ -4,13 +4,24 @@ class_name TalkComponent
 @export var conversation_sequence: Array[DialogueData]
 @export var interaction_component: InteractionComponent
 
+@export_category("Settings")
+# 이 체크박스를 켜면 한 번만 대화하고, 끄면 무한 반복합니다.
+@export var is_one_time: bool = false
+
+var has_spoken: bool = false
+
 signal conversation_finished
 
 func _ready() -> void:
 	interaction_component.press_interaction.connect(spawn_textbox)
 	
 func spawn_textbox():
-	#var textbox = preload("res://UI/TextBox/Text_Box_UI.tscn").instantiate()
+	if is_one_time and has_spoken:
+		return
+	DialogueManager.start_dialogue(conversation_sequence, _on_dialogue_finished)
 	
-	DialogueManager.start_dialogue(conversation_sequence, func(): conversation_finished.emit())
-	#get_tree().current_scene.add_child(textbox)
+func _on_dialogue_finished():
+	conversation_finished.emit()
+	
+	# 대화가 끝났으니 '대화 했음' 도장을 찍음
+	has_spoken = true
